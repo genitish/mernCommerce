e5d631a8ac70a554e13aca607ea1119b23c7b248
Nitish <genitish@gmail.com>
2021-07-11 23:40:41 +0530

    user and auth added

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require("morgan")
-const bodyParser = require("body-parser")
-const cookieParser = require("cookie-parser")
-const expressValidator = require("express-validator")
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+require("dotenv").config();
+// import routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
 
+// app
 const app = express();
-require('dotenv').config();
 
-//routes import
-const userRouter = require("./routes/user")
+// db
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log("DB Connected"));
 
-mongoose.connect(process.env.DATABASE,{
-    useUnifiedTopology : true,
-    useNewUrlParser : true,
-    useCreateIndex : true
-}).then(()=>{
-    console.log("Db connected")
-})
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
 
-//middlewares
-
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(expressValidator())
-
-
-//routes middleware
-app.use(userRouter)
-
+// routes middleware
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 const port = process.env.PORT || 8000;
-app.listen(port, ()=>{
-    console.log("App is running")
-}) 
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- validator/index.js
+++ validator/index.js
@@ -4,20 +4,19 @@ exports.userSignupValidator = (req, res, next) => {
         .matches(/.+\@.+\..+/)
         .withMessage("Email must contain @")
         .isLength({
-            min : 4,
-            max : 32
+            min: 4,
+            max: 32
         });
     req.check("password", "Password is required").notEmpty();
     req.check("password")
-        .isLength({ min : 6 })
-        .withMessage("Password must contain as least 6 characters")
+        .isLength({ min: 6 })
+        .withMessage("Password must contain at least 6 characters")
         .matches(/\d/)
-        .withMessage("Password must contain a digit");
+        .withMessage("Password must contain a number");
     const errors = req.validationErrors();
-    if(errors){
+    if (errors) {
         const firstError = errors.map(error => error.msg)[0];
-        return res.status(400).json({ error : firstError })
+        return res.status(400).json({ error: firstError });
     }
     next();
-
-}
+};
